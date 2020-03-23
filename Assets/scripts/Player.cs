901f813cfe587a7eabbddae542a664bb9673eb49
Aleksandar Djordjevic <acasax@gmail.com>
2020-03-23 17:08:47 +0100

    23/3/2020
    Sponer start

---
+++ Assets/scripts/Player.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class Player : MonoBehaviour
+{
+    private Vector2 targetPos;
+    public float Xincrement; //broj za koliko se pomera levo ili desno lik 
+    public float speed;
+    public float maxHeight;
+    public float minHeight;
+    public int health = 3;
+    void Start() {
+
+        targetPos = new Vector2(0, -3);   
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(health <= 0){
+            SceneManager.LoadScene("Menu");
+        }
+
+        transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
+
+        if(Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < maxHeight){
+
+            targetPos = new Vector2(transform.position.x + Xincrement, transform.position.y); 
+            transform.position = targetPos;
+
+        }else if(Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > minHeight){
+
+            targetPos = new Vector2(transform.position.x - Xincrement, transform.position.y); 
+            transform.position = targetPos;
+
+        }
+    }
+}
---
+++ Assets/scripts/Spawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject[] ObstaclePatterns;
+    private float timeBrwSpawn;
+    public float startTimeBtwSpawn;
+    public float decreaseTime;
+    public float minTime = 0.65f;
+
+    private void Update(){
+
+        if( timeBrwSpawn <= 0)
+        {
+            int rand = Random.Range(0, ObstaclePatterns.Length);
+            Instantiate(ObstaclePatterns[rand], transform.position, Quaternion.identity);
+            timeBrwSpawn = startTimeBtwSpawn;
+            if(startTimeBtwSpawn > minTime)
+            {
+                startTimeBtwSpawn -= decreaseTime;
+            }   
+        }
+        else
+        {
+            timeBrwSpawn -= Time.deltaTime;
+        }
+    }
+}
