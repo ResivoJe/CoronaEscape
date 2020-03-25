7abc0e670924d6028fd64154292591184b90c142
Aleksandar Djordjevic <acasax@gmail.com>
2020-03-25 19:20:01 +0100

    25/3/2020
    asd

--- Assets/scripts/Obstacle.cs
+++ Assets/scripts/Obstacle.cs
@@ -6,12 +6,15 @@ public class Obstacle : MonoBehaviour
 {
      public int damage = 1;
      public float speed;
+    public GameObject effect;
+
     private void Update() {
      transform.Translate(Vector2.down * speed * Time.deltaTime);   
     }
    
     void OnTriggerEnter2D(Collider2D other) {
         if(other.CompareTag("Player")){ // Pravljenje demidza
+            Instantiate(effect, transform.position, Quaternion.identity);
             other.GetComponent<Player>().health -= damage;
             Debug.Log(other.GetComponent<Player>().health);
             Destroy(gameObject);
