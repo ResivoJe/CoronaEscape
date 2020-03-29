ae2edc9cff4f93b8403932e43a80e27582fa5475
Aleksandar Djordjevic <acasax@gmail.com>
2020-03-29 19:38:09 +0200

    29/3/2020
    Android and gameplayfinish

--- Assets/scripts/Player.cs
+++ Assets/scripts/Player.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 
 public class Player : MonoBehaviour
@@ -12,31 +13,56 @@ public class Player : MonoBehaviour
     public float maxHeight;
     public float minHeight;
     public int health = 3;
+    public Text healthDisplay;   
+    public GameObject playAgain;
+    public GameObject infectedPlayer1;
+    public Sprite infectedPlayer;
+    bool status = true;
+    private float ScreenWidth;
+
+    public GameObject character;
+    private Rigidbody2D characterbody;
+
     void Start() {
-
         targetPos = new Vector2(0, -3);   
-
+        //playAgain = GameObject.Find("PlayAgain");
+        ScreenWidth = Screen.width;
+        characterbody = character.GetComponent<Rigidbody2D>();
+        playAgain.SetActive(false);  
     }
 
     // Update is called once per frame
     void Update()
     {
+        healthDisplay.text = health.ToString();
+
         if(health <= 0){
-            SceneManager.LoadScene(0);
+            playAgain.SetActive(true);
+            Time.timeScale = 0f;
+            infectedPlayer1.GetComponent<SpriteRenderer>().sprite = infectedPlayer;
+            if(status == true){
+         
+                transform.Rotate (0, 0, 75);  
+                status = false;
+            }
+           
         }
+
+        int j = 0;
 
         transform.position = Vector2.MoveTowards(transform.position, targetPos, speed * Time.deltaTime);
 
-        if(Input.GetKeyDown(KeyCode.RightArrow) && transform.position.x < maxHeight){
-
-            targetPos = new Vector2(transform.position.x + Xincrement, transform.position.y); 
-            transform.position = targetPos;
-
-        }else if(Input.GetKeyDown(KeyCode.LeftArrow) && transform.position.x > minHeight){
-
-            targetPos = new Vector2(transform.position.x - Xincrement, transform.position.y); 
-            transform.position = targetPos;
-
+        while(j < Input.touchCount){
+            if(Input.GetTouch(j).position.x > ScreenWidth / 2 && transform.position.x < maxHeight ){
+                targetPos = new Vector2(transform.position.x + Xincrement, transform.position.y); 
+                transform.position = targetPos;
+            }
+            if(Input.GetTouch(j).position.x < ScreenWidth / 2 && transform.position.x > minHeight){
+                targetPos = new Vector2(transform.position.x - Xincrement, transform.position.y); 
+                transform.position = targetPos;
+            }
+            j++;
         }
+      
     }
 }
--- Assets/scripts/ScoreManager.cs
+++ Assets/scripts/ScoreManager.cs
@@ -1,19 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScoreManager : MonoBehaviour
 {
     public int score;
     public int i = 0;
+    public Text scoreDisplay;
+
+    private void Update() {
+        scoreDisplay.text = score.ToString();    
+    }
     private void OnTriggerEnter2D(Collider2D other) {
         i++;
-    if(other.CompareTag("Obstacle")){
-      if(i == 4){
-          score++;
-          i = 0;
-          Debug.Log(score);
-      }
-    }   
-   }
+        if(other.CompareTag("Obstacle")){
+            if(i == 4){
+                score++;
+                i = 0;
+                Debug.Log(score);
+            }
+        }   
+    }
 }
